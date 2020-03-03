6fe755724ee92ccc14ec6e27aa3e716d87a53c9a
ukari <muromi.ukari@gmail.com>
2020-03-04 04:09:11 +0800

    add parseFile

--- src/Todo/Parse.hs
+++ src/Todo/Parse.hs
@@ -1,3 +1,20 @@
 module Todo.Parse
-  (
+  ( parseFile
   ) where
+
+import Todo.Ast
+import Text.Megaparsec
+import Data.Text.Lazy (Text)
+import Data.Text.Lazy.IO as T
+import Data.Void
+import Control.Monad.IO.Class (MonadIO, liftIO)
+
+parseFile :: MonadIO m => FilePath -> m ([Exp Text])
+parseFile filepath = liftIO $ do
+  source <- T.readFile filepath
+  result <- return $ parse expsParser filepath source
+  return $ f result
+  where
+    f :: Either (ParseErrorBundle Text Void) [Exp Text] -> [Exp Text]
+    f (Left x) = error $ errorBundlePretty x
+    f (Right x) = x
