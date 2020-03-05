e1f185bea4b1324bca93f1b3639d7b16fc433e99
ukari <muromi.ukari@gmail.com>
2020-03-05 14:11:43 +0800

    update formatter

--- src/Todo/Formatter.hs
+++ src/Todo/Formatter.hs
@@ -6,7 +6,9 @@ import Todo.Eval
 import Data.Text.Lazy (Text)
 import Data.Text.Lazy.Encoding (encodeUtf8)
 import Data.ByteString.Lazy.UTF8 (toString)
+import Data.List (intercalate)
 
 format :: Either EvalException [Text] -> String
-format (Left ex) = show ex <> "\n"
-format (Right exps) = foldl (\acc (idx, cur) -> acc <> "~> " <> (show (idx::Int)) <> ". " <> (toString $ encodeUtf8 cur) <> "\n") "" $ zip [0..] exps
+format (Left ex) = show ex
+format (Right exps) = intercalate "\n" $
+  map (\(idx, cur) -> "~> " <> (show (idx::Int)) <> ". " <> (toString $ encodeUtf8 cur)) $ zip [0..] exps
