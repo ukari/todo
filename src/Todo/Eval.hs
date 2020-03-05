b01d0d0a5d0f8220fcf18131ea3d1c6db6b6a007
ukari <muromi.ukari@gmail.com>
2020-03-05 10:29:17 +0800

    add check, update evaluate

--- src/Todo/Eval.hs
+++ src/Todo/Eval.hs
@@ -1,19 +1,43 @@
 {-# LANGUAGE GADTs #-}
 
 module Todo.Eval
-  ( evaluate
+  ( EvalException (..)
+  , evaluate
   ) where
 
 import Todo.Ast
 import Data.Text.Lazy (Text)
 import Data.Sequence
 import Data.Foldable
+import Control.Exception (Exception)
+import Control.Monad
 
 plain :: Exp Text -> Text
 plain (Task t) = t
 plain (Todo e) = plain e
 plain (Undo e) = plain e
 plain (Done e) = plain e
+
+data EvalException
+  = UnexceptedTaskException
+  | IllegalTodoException
+  | IllegalUndoException
+  | IllegalDoneException
+  deriving (Show)
+
+instance Exception EvalException
+
+check :: Exp Text -> Seq Text -> Either EvalException (Exp Text)
+check (Task t) _ = Left UnexceptedTaskException
+check (Todo e) exps = case (elemIndexL (plain e) exps) of
+  Nothing -> Right (Todo e)
+  Just _ -> Left IllegalTodoException
+check (Undo e) exps = case (elemIndexL (plain e) exps) of
+  Nothing -> Right (Undo e)
+  Just _ -> Left IllegalUndoException
+check (Done e) exps = case (elemIndexL (plain e) exps) of
+  Just _ -> Right (Done e)
+  Nothing -> Left IllegalDoneException
 
 eval :: Exp Text -> Seq Text -> Seq Text
 eval (Task t) exps = exps |> t
@@ -25,8 +49,13 @@ eval (Done e) exps = done (elemIndexL (plain e) exps) exps
     done (Just idx) s = deleteAt idx s
     done Nothing s = s
 
-evaluate :: [Exp Text] -> [Text]
-evaluate xs = toList $ foldl f empty xs
+evaluate :: [Exp Text] -> Either EvalException [Text]
+evaluate xs = do
+  case (foldM f empty xs) of
+    Left x -> Left x
+    Right x -> Right $ toList x
   where
-    f :: Seq Text -> Exp Text -> Seq Text
-    f acc cur = eval cur acc
+    f :: Seq Text -> Exp Text -> Either EvalException (Seq Text)
+    f acc cur = do
+      action <- (check cur acc)
+      return $ eval action acc
