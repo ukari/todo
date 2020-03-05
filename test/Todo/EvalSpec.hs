b80a86c145cffd6062e015b75245e3fb48e28fd1
ukari <muromi.ukari@gmail.com>
2020-03-05 10:48:26 +0800

    add test case for illegal undo

--- test/Todo/EvalSpec.hs
+++ test/Todo/EvalSpec.hs
@@ -9,18 +9,25 @@ import Data.Text.Lazy (Text, pack)
 
 source :: [Exp Text]
 source =
-  [ Todo . Task $ pack "test 0"
-  , Undo . Task $ pack "test 1"
-  , Todo . Task $ pack "test 2"
-  , Done . Task $ pack "test 0"
-  , Todo . Task $ pack "test 3"
-  , Done . Task $ pack "test 1"]
+  [ Todo $ Task $ pack "test 0"
+  , Undo $ Task $ pack "test 1"
+  , Todo $ Task $ pack "test 2"
+  , Done $ Task $ pack "test 0"
+  , Todo $ Task $ pack "test 3"
+  , Done $ Task $ pack "test 1"]
 
-result :: [Text]
-result = map pack ["test 2","test 3"]
+source_bad :: [Exp Text]
+source_bad =
+  [ Todo $ Task $ pack "task 0"
+  , Done $ Task $ pack "task 0"
+  , Todo $ Task $ pack "task 0"
+  , Undo $ Task $ pack "task 0" ]
 
 spec :: Spec
 spec = do
   describe "Eval" $ do
-    it "test evaluate" $ do
-      (evaluate source) `shouldBe` result 
+    it "normal evaluate" $ do
+      (evaluate source) `shouldBe` (Right $ map pack ["test 2","test 3"])
+
+    it "illegal undo action" $ do
+      (evaluate source_bad) `shouldBe` Left IllegalUndoException
