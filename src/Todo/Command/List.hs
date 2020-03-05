cfed1663b72c067a87c5245d8f6bd2a5f995868a
ukari <muromi.ukari@gmail.com>
2020-03-05 14:14:52 +0800

    update command list

--- src/Todo/Command/List.hs
+++ src/Todo/Command/List.hs
@@ -2,5 +2,13 @@ module Todo.Command.List
   ( list
   ) where
 
+import Todo.Parse
+import Todo.Eval
+import Todo.Formatter
+import qualified Todo.Logger as Logger
+
 list :: FilePath -> IO ()
-list filepath = print "list"
+list filepath = do
+  ast <- parseFile filepath
+  exps <- return $ evaluate ast
+  Logger.log $ format exps
