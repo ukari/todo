c9293392b46aa6ede854c98a6c0a0b599b253028
ukari <muromi.ukari@gmail.com>
2020-03-06 15:11:47 +0800

    command rollback

--- src/Todo/Command/Rollback.hs
+++ src/Todo/Command/Rollback.hs
@@ -1,6 +1,32 @@
+{-# LANGUAGE QuasiQuotes, ScopedTypeVariables #-}
+
 module Todo.Command.Rollback
   ( rollback
   ) where
 
+import Todo.Ast
+import Todo.Parse
+import Todo.Generate
+import qualified Todo.Logger as Logger
+import Data.Text.Lazy (Text)
+import qualified Data.Text.Lazy.IO as T
+import Text.InterpolatedString.QM
+import Data.List (genericLength)
+import Control.Exception (catch, SomeException)
+
 rollback :: FilePath -> IO ()
-rollback filepath = print "rollback"
+rollback filepath = do
+  ast <- parseFile filepath
+  if
+    (length ast > 0)
+  then do
+    latest <- return $ last ast
+    tryRoll latest ast
+  else
+    Logger.log "Error no actions left to rollback."
+  where
+    tryRoll :: Exp Text -> [Exp Text] -> IO ()
+    tryRoll (Undo _) _ = Logger.log "Error can't rollback."
+    tryRoll l ast = do
+      T.writeFile filepath $ generate $ take ((genericLength ast) - 1) ast
+      Logger.log [qm|Last [{action l}] action revert.|]
