c289132213cf525f698561e007db6a6b4aff918a
ukari <muromi.ukari@gmail.com>
2020-03-06 15:11:23 +0800

    add generate.action

--- src/Todo/Generate.hs
+++ src/Todo/Generate.hs
@@ -1,12 +1,13 @@
 {-# LANGUAGE GADTs, QuasiQuotes #-}
 
 module Todo.Generate
-  ( gene
+  ( action
+  , gene
   , generate
   ) where
 
 import Todo.Ast
-import Data.Text.Lazy (Text, empty)
+import Data.Text.Lazy (Text, empty, pack)
 import Text.InterpolatedString.QM
 
 gene :: Exp Text -> Text
@@ -14,6 +15,11 @@ gene (Task t) = t
 gene (Todo e) = [qm|todo "{gene e}"\n|]
 gene (Undo e) = [qm|undo "{gene e}"\n|]
 gene (Done e) = [qm|done "{gene e}"\n|]
+
+action :: Exp Text -> Text
+action (Todo _) = pack "add"
+action (Done _) = pack "fine"
+action _ = error "shouldn't run here"
 
 generate :: [Exp Text] -> Text
 generate exps = foldl f empty exps
