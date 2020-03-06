7a27b51a4a7a4a5e8d2a03be619cda4828d9aa2c
ukari <muromi.ukari@gmail.com>
2020-03-06 10:55:44 +0800

    command gc

--- src/Todo/Command/Gc.hs
+++ src/Todo/Command/Gc.hs
@@ -1,6 +1,31 @@
+{-# LANGUAGE QuasiQuotes #-}
+
 module Todo.Command.Gc
   ( gc
   ) where
 
+import Todo.Ast
+import Todo.Parse
+import Todo.Eval
+import Todo.Format
+import Todo.Generate
+import qualified Todo.Logger as Logger
+import Data.Text.Lazy (Text)
+import qualified Data.Text.Lazy.IO as T
+import Data.Either.Combinators
+import Text.InterpolatedString.QM
+import Data.List (genericLength)
+
 gc :: FilePath -> IO ()
-gc filepath = print "gc"
+gc filepath = do
+  ast <- parseFile filepath
+  exps <- return $ evaluate $ ast
+  whenRight exps $ gc' filepath
+  ast_after <- parseFile filepath
+  gcReport $ (genericLength ast) - (genericLength ast_after)
+
+gc' :: FilePath -> [Text] -> IO ()
+gc' filepath ts = T.writeFile filepath $ generate $ map (Undo . Task) ts
+
+gcReport :: Integer -> IO ()
+gcReport i = Logger.log [qm|[{i} history actions] collected.|]
