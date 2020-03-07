3f6b04e6d07a4c96f9e3c3fc7ba8e9c5cf38d7fd
ukari <muromi.ukari@gmail.com>
2020-03-08 04:39:57 +0800

    refactor Todo.Parse, provide Todo.Parse.parse

--- src/Todo/Parse.hs
+++ src/Todo/Parse.hs
@@ -1,9 +1,11 @@
 module Todo.Parse
-  ( parseFile
+  ( parse
+  , parseFile
   ) where
 
 import Todo.Ast
-import Text.Megaparsec
+import Text.Megaparsec (ParseErrorBundle, errorBundlePretty)
+import qualified Text.Megaparsec as Parsec
 import Data.Text.Lazy (Text)
 import Data.Text.Lazy.IO as T
 import Data.Void
@@ -12,7 +14,11 @@ import Control.Monad.IO.Class (MonadIO, liftIO)
 parseFile :: MonadIO m => FilePath -> m ([Exp Text])
 parseFile filepath = liftIO $ do
   source <- T.readFile filepath
-  result <- return $ parse expsParser filepath source
+  parse source filepath
+
+parse :: MonadIO m => Text -> FilePath -> m ([Exp Text])
+parse source filepath = do
+  result <- return $ Parsec.parse expsParser filepath source
   return $ f result
   where
     f :: Either (ParseErrorBundle Text Void) [Exp Text] -> [Exp Text]
