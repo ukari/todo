b656c5529498edd6c3f0cfd2e70a360f702a8fc6
ukari <muromi.ukari@gmail.com>
2020-03-08 06:29:35 +0800

    refactor

--- src/Todo/Generate.hs
+++ src/Todo/Generate.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE GADTs, QuasiQuotes #-}
+{-# LANGUAGE GADTs, QuasiQuotes, LambdaCase #-}
 
 module Todo.Generate
   ( action
@@ -11,25 +11,27 @@ import Data.Text.Lazy (Text, empty, pack)
 import qualified Data.Text.Lazy as Text
 import Text.InterpolatedString.QM
 
-escapeChars :: [Char]
-escapeChars = [ '\0'
-              , '\a'
-              , '\b'
-              , '\f'
-              , '\n'
-              , '\r'
-              , '\t'
-              , '\v'
-              , '\"'
-            --, '\&' means empty string
-              , '\''
-              , '\\']
+isEscape :: Char -> Bool
+isEscape = \case
+  '\0' -> True -- U+0000 null character
+  '\a' -> True -- U+0007 alert
+  '\b' -> True -- U+0008 backspace
+  '\f' -> True -- U+000C form feed
+  '\n' -> True -- U+000A newline (line feed)
+  '\r' -> True -- U+000D carriage return
+  '\t' -> True -- U+0009 horizontal tab
+  '\v' -> True -- U+000B vertical tab
+  '\"' -> True -- U+0022 double quote
+--'\&' -> True -- n/a    empty string
+  '\'' -> True -- U+0027 single quote
+  '\\' -> True -- U+005C backslash
+  _ -> False
 
 gene :: Exp Text -> Text
 gene (Task t) = Text.foldl escape empty t
   where
     escape :: Text -> Char -> Text
-    escape acc cur = if elem cur escapeChars then acc <> pack ['\\', cur] else acc <> pack [cur]
+    escape acc cur = if isEscape cur then acc <> pack ['\\', cur] else acc <> pack [cur]
 gene (Todo e) = [qm|todo "{gene e}"\n|]
 gene (Undo e) = [qm|undo "{gene e}"\n|]
 gene (Done e) = [qm|done "{gene e}"\n|]
